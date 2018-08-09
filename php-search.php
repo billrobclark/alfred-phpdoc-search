761d4fc5664aaf32646871592553ee2ada821285
Bill Clark <william.clark5001@gmail.com>
2018-08-09 10:53:14 -0500

    Finish out the rest of the script

--- php-search.php
+++ php-search.php
@@ -11,3 +11,21 @@ $workflow = new Workflow;
 $algoliaClient = new Algolia('A6XQ78SBYL', '6b55caa922558f0094b3aba94b293d0c');
 
 $index = $algoliaClient->initIndex('php-docs');
+
+$search = $index->search($query);
+$results = $search['hits'];
+
+foreach ($results as $hit) {
+    $title = strip_tags(html_entity_decode($hit['_highlightResult']['title']['value'], ENT_QUOTES, 'UTF-8'));
+
+    $workflow->result()
+        ->uid($hit['objectID'])
+        ->title($title)
+        ->autocomplete($title)
+        ->subtitle($hit['subtext'])
+        ->arg($hit['link'])
+        ->quicklookurl($hit['link'])
+        ->valid(true);
+}
+
+echo $workflow->output();
